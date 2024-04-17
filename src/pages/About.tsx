c79559eb7e948e192401a2f6e7a90b3e6aebd030
danivegara <72407435+danivegara@users.noreply.github.com>
2024-04-18 00:06:13 +0100

    Lower sized image files. She sounds off added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,15 +23,15 @@ const About = () => {
 
                     <div className='font-thin text-sm'>
                         <p className="pb-5">
-                            Hi! I'm Julia. I have always been fascinated by different media and how they can be used to enhance the stories they present. In my jobs and passion projects, I have long been trying to learn about as many media forms as I can — to get a thorough overview of what can be done. 
+                            Hi! I'm Julia. I have always been fascinated by different media and how they can be used to enhance stories. Through my professional and personal projects, I have explored many media forms, giving me a thorough overview of what can be done. 
                         </p>
             
                         <p className="pb-5">
-                            Because of this, I am deeply rooted in the creative industry. Apart from experience, you can read about below, I managed to dive deeper even into these fields during my degrees in Journalism and Management of Arts & Culture. After these five years, I bring a distinctive perspective to my work, blending creativity with strategic communications and management.
+                            Because of this, I am deeply rooted in the creative industry. Apart from the experience you can read about below, I dove deeper into these fields during my degrees in Journalism and Management of Arts & Culture. I bring a distinctive perspective to my work, blending creativity with strategic communications and management.
                         </p>
             
                         <p className="pb-0">
-                            My goal is to one day be in a position that would allow me to create projects and campaigns about important world issues through art, music, the written word, and emerging media. I am particularly interested in gender equality, climate change action, access to education, and equal representation. Combining those with my love for art and culture would be a dream.
+                           I curate projects and campaigns about important world issues through art, music, the written word, and emerging media. I am particularly interested in gender equality, climate change action, and access to education. It's when I get to combine these topics with creative storytelling that I do my best work.
                         </p>
             
                         <div className="flex flex-row space-x-1">
